a42539cb5de8ec274d4e2c0608dfddfc1b4485cc
Roel Zinkstok <rzinkstok@gmail.com>
2019-02-19 09:54:44 +0100

    Adds test-input-data

--- eyra_submission/template/{{ cookiecutter.package_name }}/test.sh
+++ eyra_submission/template/{{ cookiecutter.package_name }}/test.sh
@@ -4,7 +4,7 @@
 
 docker volume create {{ cookiecutter.package_name|lower }}-output
 
-docker run --rm --memory=4g -v $(pwd)/test/:/input/ -v {{ cookiecutter.package_name|lower }}-output:/output/ {{ cookiecutter.package_name|lower }}
+docker run --rm --memory=4g -v $(pwd)/test-input-data/:/input/ -v {{ cookiecutter.package_name|lower }}-output:/output/ {{ cookiecutter.package_name|lower }}
 
 docker run --rm -v {{ cookiecutter.package_name|lower }}-output:/output/ {{ cookiecutter.docker_base_image }} cat /output/data.txt
 
