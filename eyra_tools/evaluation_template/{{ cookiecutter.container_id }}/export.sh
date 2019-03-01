74b00378668094572d89143326e6020796ee4951
Roel Zinkstok <rzinkstok@gmail.com>
2019-03-01 15:47:14 +0100

    Removes export scripts

--- eyra_tools/evaluation_template/{{ cookiecutter.container_id }}/export.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./build.sh
-
-docker save {{ cookiecutter.container_id }} > {{ cookiecutter.container_id }}.tar | gzip > {{ cookiecutter.container_id }}.tar.gz
-if [ -e {{ cookiecutter.container_id }}.tar ]
-then
-    rm {{ cookiecutter.container_id }}.tar
-fi
