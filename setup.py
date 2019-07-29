b6e6da5679f49fb11e444cb0b75002d5dd8e3e43
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-07-29 16:13:50 +0200

    Add more metadata about the package

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ setup(
             ]
     },
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
