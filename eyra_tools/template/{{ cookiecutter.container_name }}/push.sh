d89cc66617916adf50a7ef3ff5ee0e3fefa9ed2b
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-08-26 17:31:39 +0200

    Optionally specify tag before pushing docker image
    If the tag isn't specified. 'latest' is used.
    It would be nice to test this, but I'm not sure how (because it involves
    pushing to docker hub).

--- eyra_tools/template/{{ cookiecutter.container_name }}/push.sh
+++ eyra_tools/template/{{ cookiecutter.container_name }}/push.sh
@@ -2,5 +2,9 @@
 
 ./build.sh
 
-docker tag {{ cookiecutter.container_name }} {{ cookiecutter.docker_hub_account }}/{{ cookiecutter.container_name }}
+TAG=${1:-latest}
+
+echo "Using tag: $TAG."
+
+docker tag {{ cookiecutter.container_name }} {{ cookiecutter.docker_hub_account }}/{{ cookiecutter.container_name }}:$TAG
 docker push {{ cookiecutter.docker_hub_account }}/{{ cookiecutter.container_name }}
